af96c78d93692af627aa7928d00aca40a913edd7
olegpel <oleg_pelishok@yahoo.com>
2015-12-20 18:29:31 +0200

    first commit

---
+++ contacktbook/Contact.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace contacktbook
+{
+    /// <summary>
+    /// Unit  of contact book
+    /// </summary>
+    [Serializable]
+    public class Contact
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string Tel1 { get; set; }
+        public string Tel2 { get; set; }
+        public string Email { get; set; }
+        //public DateTime DateOfBirth { get; set; }
+
+        public Contact()
+        {}
+
+        public Contact(string id, string firstname, string secondName, string tel1,
+            string tel2, string email)
+        {
+            Id = id;
+            FirstName = firstname;
+            SecondName = secondName;
+            Tel1 = tel1;
+            Tel2 = tel2;
+            Email = email;
+        }
+    }
+
+}
---
+++ contacktbook/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Xml.Serialization;
+
+
+namespace contacktbook
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSwitch_Click(object sender, RoutedEventArgs e)
+        {
+            TheSecondWindow secondWindow = new TheSecondWindow();
+            secondWindow.Show();
+            this.Hide();
+        }
+
+
+        private void Parse_Click(object sender, RoutedEventArgs e)
+        {
+
+            Contact contact = new Contact("1", "Sokhan", "Oleg", "0671111111", "6916", "oleg.sokhan@rosan-it.com");
+            Contact contact1 = new Contact("2", "Taras", "Roman", "0671111111", "6915","taras.roman@rosan-it.com");
+            Contact [] AllContacts = new Contact [] {contact,contact1};
+            XmlSerializer formatter = new XmlSerializer(typeof(Contact[]));
+            using (FileStream fs = new FileStream("D:\\contact.xml", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, AllContacts);
+            }
+        }
+
+        private void AddObject_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+
+    }
+}
+
+//private List<Contact> ParseXmlToContactList(string path)
+//{
+//    List<Contact> resultList;
+
+//    using (var reader = new StreamReader(path))
+//    {
+//        XmlSerializer deserializer = new XmlSerializer(typeof(List<Contact>), new XmlRootAttribute("Contacts"));
+//        resultList = (List<Contact>)deserializer.Deserialize(reader);
+//    }
+//    return resultList;
+//}
